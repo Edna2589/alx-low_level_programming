9a51611a7427dbba92499a3794ff375e373a6002
Edna2589 <solomonedna1244@gmail.com>
2023-03-17 03:35:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
-#include <stdio.h
+#include <stdio.h>
 /**
  * main A program that prints a line with function
  * Return: 0
+ */
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzz");
+return (0);
+}
