9d2e89eade12d214f39c80a808e42ab345f63cfa
Edna2589 <solomonedna1244@gmail.com>
2023-03-21 08:56:51 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - checks whether character passed is lowercase
+ *
+ * Return: 1 if @c is lowercase otherwise 0
+ */
+int _isolower(int c)
+{
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0);
+}
